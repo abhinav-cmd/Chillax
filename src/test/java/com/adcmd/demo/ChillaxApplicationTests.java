492f7a4c32d09614c60d24f649f8f590534f6976
Abhinav <abhinavsvvv@gmail.com>
2021-03-15 11:22:58 +0530

    commited from desktop

---
+++ src/test/java/com/adcmd/demo/ChillaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adcmd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChillaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
